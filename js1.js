3355e81a0966a0ddd8e95bc74fdec94850783a0a
Max-RichardPohl <Max-RichardPohl@web.de>
2022-05-22 19:09:40 +0200

    commit
    all files

---
+++ js1.js
@@ -0,0 +1,48 @@
+function Automat (Wort) {
+var Wort = document.Formular.Eingabe.value
+var t = 2
+var s = 2
+if (Wort == "BTXSE")
+{alert(Wort + " passt zur Sprache. :-)")}
+    else if (Wort[0] == "B" && Wort[1] == "T"){
+      switch (Wort[2])
+      {
+        case "S":
+          while (Wort[s] == "S")
+            {
+              s++
+            }
+            if (Wort[s] == "X" && Wort [s+1] == "S" && Wort [s+2] == "E")
+            {
+              alert(Wort + " passt zur Sprache. :-)")
+              break;
+            }
+        default:
+          alert(Wort + " passt nicht zur Sprache.")
+      }
+    }
+    else if (Wort == "BPVVE")
+      {alert(Wort + " passt zur Sprache. :-)")}
+    else if (Wort[0] == "B" && Wort[1] == "P"){
+      switch (Wort[2])
+      {
+        case "T":
+          while (Wort[t] == "T")
+            {
+              t++
+            }
+            if (Wort[t] == "X" && Wort[t+1] == "S" && Wort[t+2] == "E")
+            {
+              alert(Wort + " passt zur Sprache. :-)")
+              break;
+            }
+            else if (Wort[t] == "V" && Wort[t+1] == "P" && Wort[t+2] == "S" && Wort[t+3] == "E") {
+              alert(Wort + " passt zur Sprache. :-)")
+              break;
+            }
+        default:
+          alert(Wort + " passt nicht zur Sprache.")
+        }
+    }
+else {alert(Wort + " passt nicht zur Sprache.")}
+}
